ce6d1df4b42af528ee11b516fe0ff58b4ea44793
Nathan T. Baker <nathantbaker@gmail.com>
2016-10-19 15:36:11 -0500

    passed object into makeTree fuction, not variables

--- quiz.js
+++ quiz.js
@@ -4,10 +4,6 @@ var inputObject = {
   character: ""
 };
 
-// Reference to Object elements
-var height = inputObject.height;
-var characer = inputObject.character;
-
 // Reference to elements on page
 var formElement = document.getElementById("form"); //to listen for field inputs
 var heightElement = document.getElementById("height");
@@ -29,36 +25,41 @@ formElement.addEventListener("keyup", function(event) {
 
 // Function to push user inputs into object
 function pushInputs() {
+console.log("pushInputs fuction running");
   // Save new inputs
-  height = heightElement.value;
-  character = characterElement.value;
+  inputObject.height = heightElement.value;
+  inputObject.character = characterElement.value;
   // Error if either of new inputs are nothing
-  if (character == "" || height == "") {
+  if (inputObject.character == "" || inputObject.height == "") {
     alert(alertMissingInput);
   } else {
-    makeTree(height, character);
+    console.log("inputObject:", inputObject);
+    makeTree(inputObject);
   }
 }
 
 // Function to draw tree in Console
-function makeTree(treeHeight, treeCharacter) {
+function makeTree(treeObject) {
+  console.log("running makeTree function");
+  console.log("height:", treeObject.height);
   var spacesString = "";
   var charactersString = "";
   var space = " ";
 
   // Set spaces in spacesString to height
-  for (var j = 0; j < treeHeight; j++) {
+  for (var j = 0; j < treeObject.height; j++) {
     spacesString += space;
+    console.log("treeObject.height:", treeObject.height);
   }
 
   // Number of lines we're printing
-  for (var i = 0; i < treeHeight; i++) {
+  for (var i = 0; i < treeObject.height; i++) {
     // Remove a space each line
     spacesString = spacesString.slice(0, -1);
     // Increase length of charactersString by 1 each time.
-    charactersString += treeCharacter;
+    charactersString += treeObject.character;
     console.log(spacesString + charactersString);
     // Increase length of characters by 1 each time AFTER console log
-    charactersString += treeCharacter;
+    charactersString += treeObject.character;
   }
 }
